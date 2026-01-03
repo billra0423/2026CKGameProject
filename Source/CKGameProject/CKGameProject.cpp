746e740b333345b36935ed2529d183d63eb62fc8
billra0423 <billra0423@gmail.com>
2026-01-03 17:38:09 +0900

    프로젝트 시작

---
+++ Source/CKGameProject/CKGameProject.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CKGameProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CKGameProject, "CKGameProject" );
+
+DEFINE_LOG_CATEGORY(LogCKGameProject)
---
+++ Source/CKGameProject/CKGameProjectGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CKGameProjectGameMode.h"
+
+ACKGameProjectGameMode::ACKGameProjectGameMode()
+{
+	// stub
+}
